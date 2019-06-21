c1ac584f955affb82645badecb8245a31037bd42
aswea002 <asweatt18@gmail.com>
2019-06-21 13:44:33 -0400

    friends js update with friends

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friends = [
+  {
+    name: "Oprah Winfrey",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/f/fa/Oprah_Winfrey_%282004%29.jpg",
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+  },
+  {
+    name: "Will Smith",
+    photo:
+      "https://vignette.wikia.nocookie.net/unbreakablemovie/images/a/a9/476325099-actor-will-smith-attends-the-oscars-held-at-hollywood.jpg.CROP_.rtstoryvar-large.jpg/revision/latest?cb=20170904193601",
+    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+  },
+  {
+    name: "Nia Long",
+    photo: "https://images.huffingtonpost.com/2013-04-05-NiaLong219_final.jpg",
+    scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
+  },
+  {
+    name: "Samuel L. Jackson",
+    photo:
+      "https://m.media-amazon.com/images/M/MV5BMTQ1NTQwMTYxNl5BMl5BanBnXkFtZTYwMjA1MzY1._V1_UX214_CR0,0,214,317_AL_.jpg",
+    scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
+  },
+  {
+    name: "Tia Mowry Hardwick",
+    photo:
+      "https://cdn.crownmediadev.com/dims4/default/18c7c78/2147483647/crop/1658x936%2B0%2B341/resize/726x410%5E/quality/90/?url=https%3A%2F%2Fcdn.crownmediadev.com%2F8c%2F7b%2Fc06270684ea5839d63684078be27%2Fagingerbreadromance-0453-cb-rt.jpg",
+    scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
+  },
+  {
+    name: "Beyonce",
+    photo:
+    "https://pixel.nymag.com/imgs/daily/vulture/2018/03/26/26-beyonce.w700.h467.jpg",
+    scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
+  }
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
